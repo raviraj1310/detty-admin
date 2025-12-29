1b3179d134dbbf7fd6a2b89d66b66ae546d2d4b0
Prahalad2111997 <Prahaladj12345@gmail.com>
2025-12-29 17:28:31 +0530

    add pagination in the email subscription

--- services/email-subscription/email-subscription.service.js
+++ services/email-subscription/email-subscription.service.js
@@ -1,25 +1,35 @@
-import api from '@/src/axois/axois'
+import api from "@/src/axois/axois";
 
+// export const getEmailSubscriptions = async () => {
+//   try {
+//     const response = await api.get("/subscriber/get-all-news");
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching email subscriptions:", error);
+//     throw error;
+//   }
+// };
 
-export const getEmailSubscriptions = async () => {
-    try {
-        const response = await api.get('/subscriber/get-all-news');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching email subscriptions:', error);
-        throw error;
-    }
-}
-
+export const getEmailSubscriptions = async ({ page = 1, limit = 10 } = {}) => {
+  try {
+    const res = await api.get("/subscriber/get-all-news", {
+      params: { page, limit },
+    });
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 export const downloadEmailSubscriptionsCSV = async () => {
-    try {
-        const response = await api.get('/subscriber/download-csv', {
-            responseType: 'blob',
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Error downloading email subscriptions CSV:', error);
-        throw error;
-    }
-}
+  try {
+    const response = await api.get("/subscriber/download-csv", {
+      responseType: "blob",
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error downloading email subscriptions CSV:", error);
+    throw error;
+  }
+};
