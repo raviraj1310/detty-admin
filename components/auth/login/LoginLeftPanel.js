ed5b807b1f7765cdfd1421ed91b25c3373807879
Rudrika <rudrikadave20226@gmail.com>
2026-03-27 12:38:11 +0530

    update thhe bg color at login

--- components/auth/login/LoginLeftPanel.js
+++ components/auth/login/LoginLeftPanel.js
@@ -6,13 +6,13 @@ export default function LoginLeftPanel ({
   backgroundImage = '/images/logo/logo.png'
 }) {
   return (
-    <div className='relative hidden lg:block  overflow-hidden'>
+    <div className='relative hidden lg:block bg-black overflow-hidden'>
       <Image
         src={backgroundImage}
         alt='Welcome back'
         fill
         priority
-        className='object-cover'
+        className='object-contain p-10'
       />
 
       <div className='absolute inset-0 bg-gradient-to-tr from-black/30 via-black/10 to-transparent' />
